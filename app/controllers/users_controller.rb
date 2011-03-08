1c0a27933b5bdd8e4f56e6c1032ba3fe13c6013b
OZAWA Sakuro <sakuro@2238club.org>
2011-03-08 09:37:23 +0900

    Respond only to json.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,25 +19,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
 
     respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @user }
+      format.json  { render :json => @user }
     end
-  end
-
-  # GET /users/new
-  # GET /users/new.xml
-  def new
-    @user = User.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @user }
-    end
-  end
-
-  # GET /users/1/edit
-  def edit
-    @user = User.find(params[:id])
   end
 
   # POST /users
@@ -47,40 +30,33 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to(@user, :notice => 'User was successfully created.') }
-        format.xml  { render :xml => @user, :status => :created, :location => @user }
+        format.json  { render :json => @user, :status => :created, :location => @user }
       else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+        format.json  { render :json => @user.errors, :status => :unprocessable_entity }
       end
     end
   end
 
-  # PUT /users/1
-  # PUT /users/1.xml
+  # PUT /users/1.json
   def update
-    @user = User.find(params[:id])
-
+    @user = User.find(params[:records][0][:id])
     respond_to do |format|
-      if @user.update_attributes(params[:user])
-        format.html { redirect_to(@user, :notice => 'User was successfully updated.') }
-        format.xml  { head :ok }
+      if @user.update_attributes(params[:records][0])
+        format.json  { render :json => { users: [@user], success: true } }
+#head :ok }
       else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+        format.json  { render :json => @user.errors, :status => :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.xml
+  # DELETE /users/1.json
   def destroy
-    @user = User.find(params[:id])
+    @user = User.find(params[:records][0][:id])
     @user.destroy
 
     respond_to do |format|
-      format.html { redirect_to(users_url) }
-      format.xml  { head :ok }
+      format.json  { render :json => { users:[@user], success: true } }
     end
   end
 
