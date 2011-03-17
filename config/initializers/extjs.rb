e080441f9b1c2597ffb2b7b1e543881c343b7475
OZAWA Sakuro <sakuro@2238club.org>
2011-03-17 23:02:05 +0900

    Updated to Ext.js 4 pr4.

--- config/initializers/extjs.rb
+++ config/initializers/extjs.rb
@@ -1,12 +1,12 @@
 Ext4app::Application.config.action_view.javascript_expansions[:defaults] =
   %w(ext-core ext-all).map do |script|
-    version = '4.0-pr3'
+    version = '4.0-pr4'
     script += '-debug' unless ::Rails.env.production?
     File.join("ext-#{version}", script)
   end + %w(rails)
 
 Ext4app::Application.config.action_view.stylesheet_expansions[:defaults] =
   %w(ext-all).map do |script|
-    version = '4.0-pr3'
+    version = '4.0-pr4'
     File.join("ext-#{version}/resources/css", script)
   end
