1eb52fed63a826abc95499f6df04de9390b68d47
OZAWA Sakuro <sakuro@2238club.org>
2011-03-18 23:58:38 +0900

    Add updated_at.

--- public/javascripts/Ext4app/models/User.js
+++ public/javascripts/Ext4app/models/User.js
@@ -2,7 +2,8 @@ Rails.regModel('Ext4app.models.User', {
     fields: [
         {name: 'id', type: 'int'},
         {name: 'name', type: 'string'},
-        {name: 'age', type: 'int'}
+        {name: 'age', type: 'int'},
+		{name: 'updated_at', type: 'string'}
     ],
 	proxy: {
 		url: '/users'
