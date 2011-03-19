02728d1f1687827c654fb218af112c7e630d80bc
OZAWA Sakuro <sakuro@2238club.org>
2011-03-19 13:46:28 +0900

    pr4->pr5

--- config/initializers/extjs.rb
+++ config/initializers/extjs.rb
@@ -1,12 +1,12 @@
 Ext4app::Application.config.action_view.javascript_expansions[:defaults] =
   %w(ext-core ext-all).map do |script|
-    version = '4.0-pr4'
+    version = '4.0-pr5'
     script += '-debug' unless ::Rails.env.production?
     File.join("ext-#{version}", script)
   end + %w(rails)
 
 Ext4app::Application.config.action_view.stylesheet_expansions[:defaults] =
   %w(ext-all).map do |script|
-    version = '4.0-pr4'
+    version = '4.0-pr5'
     File.join("ext-#{version}/resources/css", script)
   end
