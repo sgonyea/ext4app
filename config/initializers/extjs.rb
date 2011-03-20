496f0c0252fda55ec55c378e66b2adcab5f8495d
OZAWA Sakuro <sakuro@2238club.org>
2011-03-21 00:25:50 +0900

    Moved contents of json.rb to extjs.rb

--- config/initializers/extjs.rb
+++ config/initializers/extjs.rb
@@ -1,3 +1,7 @@
+require 'active_support/json/decoding'
+ActiveSupport::JSON.backend = 'JSONGem'
+ActiveSupport.parse_json_times = false
+
 Ext4app::Application.config.action_view.javascript_expansions[:defaults] =
   %w(ext-core ext-all).map do |script|
     version = '4.0-pr5'
--- config/initializers/json.rb
+++
@@ -1,4 +0,0 @@
-# encoding: -*- UTF-8 -*-
-require 'active_support/json/decoding'
-ActiveSupport::JSON.backend = 'JSONGem'
-ActiveSupport.parse_json_times = false
