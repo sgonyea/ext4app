91392a13ccee0cc4bbe2f5706cb8629ca94b8863
OZAWA Sakuro <sakuro@2238club.org>
2011-04-05 20:51:06 +0900

    ext-core-debug is not needed.

--- config/initializers/extjs.rb
+++ config/initializers/extjs.rb
@@ -1,5 +1,5 @@
 Ext4app::Application.config.action_view.javascript_expansions[:defaults] =
-  %w(ext-core ext-all).map do |script|
+  %w(ext-all).map do |script|
     version = '4.0-beta1'
     script += '-debug' unless ::Rails.env.production?
     File.join("ext-#{version}", script)
@@ -7,6 +7,6 @@ Ext4app::Application.config.action_view.javascript_expansions[:defaults] =
 
 Ext4app::Application.config.action_view.stylesheet_expansions[:defaults] =
   %w(ext-all).map do |script|
-    version = '4.0-pr5'
+    version = '4.0-beta1'
     File.join("ext-#{version}/resources/css", script)
   end
