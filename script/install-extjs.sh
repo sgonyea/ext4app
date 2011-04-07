4ce061462b6976c0ba32aa121b3c270a995c2774
OZAWA Sakuro <sakuro@2238club.org>
2011-04-07 15:26:33 +0900

    Commenting download URL.

--- script/install-extjs.sh
+++ script/install-extjs.sh
@@ -7,5 +7,6 @@ EOF
 fi
 ARCHIVE=$1
 
+#wget http://downloads.sencha.com/extjs/ext-4.0-beta2.zip
 unzip $ARCHIVE $(unzip -l $ARCHIVE | sed -n '/^[^/][^/]*\/[^/][^/]*\.js$/s/.* //p') -d public/javascripts
 unzip $ARCHIVE $(unzip -l $ARCHIVE | sed -n '/^[^/][^/]*\/resources/s/.* //p' |egrep '(themes/images|css)/') -d public/stylesheets
