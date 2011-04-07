95d725a8c609e95dcaec22ccb2b8ed677fc8925c
OZAWA Sakuro <sakuro@2238club.org>
2011-04-07 15:26:57 +0900

    Use bootstrap.js

--- config/initializers/extjs.rb
+++ config/initializers/extjs.rb
@@ -1,12 +1,11 @@
 Ext4app::Application.config.action_view.javascript_expansions[:defaults] =
-  %w(ext-all).map do |script|
-    version = '4.0-beta1'
-    script += '-debug' unless ::Rails.env.production?
+  %w(bootstrap).map do |script|
+    version = '4.0-beta2'
     File.join("ext-#{version}", script)
   end + %w(rails)
 
 Ext4app::Application.config.action_view.stylesheet_expansions[:defaults] =
   %w(ext-all).map do |script|
-    version = '4.0-beta1'
+    version = '4.0-beta2'
     File.join("ext-#{version}/resources/css", script)
   end
