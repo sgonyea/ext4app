12e3fae771fc67ff23875b31ddb89c6b6ae9a1a8
OZAWA Sakuro <sakuro@2238club.org>
2011-04-09 22:15:35 +0900

    beta2

--- config/initializers/extjs.rb
+++ config/initializers/extjs.rb
@@ -1,12 +1,12 @@
 Ext4app::Application.config.action_view.javascript_expansions[:defaults] =
   %w(ext-all).map do |script|
-    version = '4.0-beta1'
+    version = '4.0-beta2'
     script += '-debug' unless ::Rails.env.production?
     File.join("ext-#{version}", script)
   end + %w(rails)
 
 Ext4app::Application.config.action_view.stylesheet_expansions[:defaults] =
   %w(ext-all).map do |script|
-    version = '4.0-beta1'
+    version = '4.0-beta2'
     File.join("ext-#{version}/resources/css", script)
   end
